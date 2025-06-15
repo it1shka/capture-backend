730c53236ec118b9f5b291a7d318b54974c0ec07
it1shka <2tishbel@gmail.com>
2025-06-16 00:30:17 +0200

    Initial commit

---
+++ src/main/kotlin/com/it1shka/capture/CaptureApplication.kt
@@ -0,0 +1,11 @@
+package com.it1shka.capture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CaptureApplication
+
+fun main(args: Array<String>) {
+	runApplication<CaptureApplication>(*args)
+}
---
+++ src/test/kotlin/com/it1shka/capture/CaptureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.it1shka.capture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CaptureApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
